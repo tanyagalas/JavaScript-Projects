3ca53eccd16f9612d649c9ce1fed9e364ddaa618
tanyagalas <tanyashevts@gmail.com>
2023-04-17 17:31:17 -0700

    PROJECTS SUBMISSION ASSIGNMENT
    commit

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,11 @@
+// Defining two variables with string values about birds and put them together
+var sentence1 = "Birds are amazing creatures that come in many different colors and sizes.";
+var sentence2 = " They also have unique behaviors and sounds that make them fascinating to watch.";
+var concatenated = sentence1 + sentence2;
+
+// Write a sentence to the document with the concatenated string and an expression
+document.write("The concatenated string is: " + concatenated + "<br>");
+document.write("The length of the concatenated string is: " + concatenated.length);
+
+// Display an alert with the concatenated string
+window.alert("The concatenated string is: " + concatenated);
