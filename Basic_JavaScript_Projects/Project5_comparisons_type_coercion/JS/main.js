2de6f1a7f4f96b600988742867d0794c831bf405
tanyagalas <tanyashevts@gmail.com>
2023-04-20 15:03:27 -0700

    Project5_Assignment

---
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,30 @@
+var myVariable = 5;
+document.write("The data type of myVariable is: " + typeof myVariable);
+// Combining a string and a number
+var myString = "The answer is: ";
+var myNumber = 42;
+document.write(myString + myNumber);
+var x = 5;
+var y = 10;
+var z = "10";
+// Using == operator
+var result1 = (y == z);
+document.write("Result1: " + result1);
+// Using === operator
+var result2 = (y === z);
+document.write("Result2: " + result2);
+// Using > operator
+var result3 = (y > x); 
+document.write("Result3: " + result3);
+// Using < operator
+var result4 = (x < y); 
+document.write("Result4: " + result4);
+// Using && operator
+var result5 = (x < y) && (y < z); 
+document.write("Result5: " + result5);
+// Using || operator
+var result6 = (x > y) || (y < z); 
+document.write("Result6: " + result6);
+// Using! operator
+var result7 = !(x > y); // 
+document.write("Result7: " + result7);
